924902875032f3547209d98dc9afee851f6e31ec
robert.ancell@gmail.com <>
2010-05-01 12:47:17 +1000

    Only run as root, catch signals and clean exit

--- src/lightdm.c
+++ src/lightdm.c
@@ -10,14 +10,40 @@
  */
 
 #include <glib.h>
+#include <signal.h>
+#include <unistd.h>
 
 #include "display-manager.h"
+
+static GMainLoop *loop;
+
+static void
+signal_handler (int signum)
+{
+    g_debug ("Caught %s signal, exiting", g_strsignal (signum));
+    g_main_loop_quit (loop);
+}
 
 int
 main(int argc, char **argv)
 {
-    GMainLoop *loop;
     DisplayManager *manager;
+    struct sigaction action;
+
+    action.sa_handler = signal_handler;
+    sigemptyset (&action.sa_mask);
+    action.sa_flags = 0;
+    sigaction (SIGTERM, &action, NULL);
+    sigaction (SIGINT, &action, NULL);
+    sigaction (SIGHUP, &action, NULL);
+
+    if (getuid () != 0)
+    {
+        g_print ("Only root can run Light Display Manager\n");
+        return -1;
+    }
+
+    // Change working directory?
 
     g_type_init ();
     loop = g_main_loop_new (NULL, FALSE);
