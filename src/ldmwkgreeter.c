c27a76c79ec49fd368dbe3656511940ed121ac91
robert.ancell@gmail.com <>
2010-06-13 13:09:40 +1000

    Get more JavaScript stuff working

--- src/ldmwkgreeter.c
+++ src/ldmwkgreeter.c
@@ -16,20 +16,28 @@
 
 #include "greeter.h"
 
-static Greeter *greeter;
-
 static void
-show_prompt_cb (Greeter *greeter, const gchar *text)
+show_prompt_cb (Greeter *greeter, const gchar *text, WebKitWebView *view)
 {
+    gchar *command;
+
+    command = g_strdup_printf ("show_prompt('%s')", text); // FIXME: Escape text
+    webkit_web_view_execute_script (view, command);
+    g_free (command);
 }
 
 static void
-show_message_cb (Greeter *greeter, const gchar *text)
+show_message_cb (Greeter *greeter, const gchar *text, WebKitWebView *view)
 {
+    gchar *command;
+
+    command = g_strdup_printf ("show_message('%s')", text); // FIXME: Escape text
+    webkit_web_view_execute_script (view, command);
+    g_free (command);
 }
 
 static void
-authentication_complete_cb (Greeter *greeter)
+authentication_complete_cb (Greeter *greeter, WebKitWebView *view)
 {
     if (greeter_get_is_authenticated (greeter))
         gtk_main_quit ();
@@ -42,27 +50,6 @@ timed_login_cb (Greeter *greeter, const gchar *username)
     gtk_main_quit ();
 }
 
-static void
-ldm_init_cb (JSContextRef ctx, JSObjectRef object)
-{
-}
-
-static void
-ldm_finalize_cb (JSObjectRef object)
-{
-}
-
-static JSValueRef
-test_cb (JSContextRef context,
-         JSObjectRef function,
-         JSObjectRef thisObject,
-         size_t argumentCount,
-         const JSValueRef arguments[],
-         JSValueRef *exception)
-{
-    return JSValueMakeString (context, JSStringCreateWithUTF8CString ("Hello World!"));
-}
-
 static JSValueRef
 start_authentication_cb (JSContextRef context,
                          JSObjectRef function,
@@ -71,7 +58,19 @@ start_authentication_cb (JSContextRef context,
                          const JSValueRef arguments[],
                          JSValueRef *exception)
 {
-    printf ("!\n");
+    Greeter *greeter = JSObjectGetPrivate (thisObject);
+    JSStringRef name_arg;
+    char name[1024];
+
+    // FIXME: Throw exception
+    if (!(argumentCount == 1 && JSValueGetType (context, arguments[0]) == kJSTypeString))
+        return JSValueMakeNull (context);
+
+    name_arg = JSValueToStringCopy (context, arguments[0], NULL);
+    JSStringGetUTF8CString (name_arg, name, 1024);
+    JSStringRelease (name_arg);
+
+    greeter_start_authentication (greeter, name);
     return JSValueMakeNull (context);
 }
 
@@ -82,13 +81,25 @@ provide_secret_cb (JSContextRef context,
                    size_t argumentCount,
                    const JSValueRef arguments[],
                    JSValueRef *exception)
-{  
+{
+    Greeter *greeter = JSObjectGetPrivate (thisObject);
+    JSStringRef secret_arg;
+    char secret[1024];
+
+    // FIXME: Throw exception
+    if (!(argumentCount == 1 && JSValueGetType (context, arguments[0]) == kJSTypeString))
+        return JSValueMakeNull (context);
+
+    secret_arg = JSValueToStringCopy (context, arguments[0], NULL);
+    JSStringGetUTF8CString (secret_arg, secret, 1024);
+    JSStringRelease (secret_arg);
+
+    greeter_provide_secret (greeter, secret);
     return JSValueMakeNull (context);
 }
 
 static const JSStaticFunction ldm_functions[] =
 {
-    { "test", test_cb, kJSPropertyAttributeReadOnly },  
     { "start_authentication", start_authentication_cb, kJSPropertyAttributeReadOnly },
     { "provide_secret", provide_secret_cb, kJSPropertyAttributeReadOnly },
     { NULL, NULL, 0 }
@@ -96,30 +107,26 @@ static const JSStaticFunction ldm_functions[] =
 
 static const JSClassDefinition ldm_definition =
 {
-    0,
-    kJSClassAttributeNone,
-    "LightDMClass",
-    NULL,
-
-    NULL,
-    ldm_functions,
-
-    ldm_init_cb,
-    ldm_finalize_cb
+    0,                     /* Version */
+    kJSClassAttributeNone, /* Attributes */
+    "LightDMClass",        /* Class name */
+    NULL,                  /* Parent class */
+    NULL,                  /* Static values */
+    ldm_functions,         /* Static functions */
 };
 
 static void
 window_object_cleared_cb (WebKitWebView  *web_view,
-                          WebKitWebFrame *frame)
+                          WebKitWebFrame *frame,
+                          JSGlobalContextRef context,
+                          JSObjectRef window_object,
+                          Greeter *greeter)
 {
-    JSGlobalContextRef context;
     JSClassRef ldm_class;
     JSObjectRef ldm_object;
 
-    context = webkit_web_frame_get_global_context (frame);
-
     ldm_class = JSClassCreate (&ldm_definition);
-    ldm_object = JSObjectMake (context, ldm_class, context);
+    ldm_object = JSObjectMake (context, ldm_class, greeter);
     JSObjectSetProperty (context,
                          JSContextGetGlobalObject (context),
                          JSStringCreateWithUTF8CString ("lightdm"),
@@ -129,6 +136,7 @@ window_object_cleared_cb (WebKitWebView  *web_view,
 int
 main(int argc, char **argv)
 {
+    Greeter *greeter;
     GdkDisplay *display;
     GdkScreen *screen;
     gint screen_width, screen_height;
@@ -137,14 +145,6 @@ main(int argc, char **argv)
     gtk_init (&argc, &argv);
 
     greeter = greeter_new ();
-
-    g_signal_connect (G_OBJECT (greeter), "show-prompt", G_CALLBACK (show_prompt_cb), NULL);  
-    g_signal_connect (G_OBJECT (greeter), "show-message", G_CALLBACK (show_message_cb), NULL);
-    g_signal_connect (G_OBJECT (greeter), "show-error", G_CALLBACK (show_message_cb), NULL);
-    g_signal_connect (G_OBJECT (greeter), "authentication-complete", G_CALLBACK (authentication_complete_cb), NULL);
-    g_signal_connect (G_OBJECT (greeter), "timed-login", G_CALLBACK (timed_login_cb), NULL);
-
-    greeter_connect (greeter);
 
     display = gdk_display_get_default ();
     screen = gdk_display_get_default_screen (display);
@@ -157,9 +157,17 @@ main(int argc, char **argv)
     gtk_window_move (GTK_WINDOW (window), 0, 0);
 
     web_view = webkit_web_view_new ();
-    g_signal_connect (G_OBJECT (greeter), "window-object-cleared", G_CALLBACK (window_object_cleared_cb), NULL);
-    webkit_web_view_load_uri (WEBKIT_WEB_VIEW (web_view), "file:///home/bob/bzr/lightdm/index.html");
+    g_signal_connect (G_OBJECT (web_view), "window-object-cleared", G_CALLBACK (window_object_cleared_cb), greeter);
     gtk_container_add (GTK_CONTAINER (window), web_view);
+
+    g_signal_connect (G_OBJECT (greeter), "show-prompt", G_CALLBACK (show_prompt_cb), web_view);
+    g_signal_connect (G_OBJECT (greeter), "show-message", G_CALLBACK (show_message_cb), web_view);
+    g_signal_connect (G_OBJECT (greeter), "show-error", G_CALLBACK (show_message_cb), web_view);
+    g_signal_connect (G_OBJECT (greeter), "authentication-complete", G_CALLBACK (authentication_complete_cb), web_view);
+    g_signal_connect (G_OBJECT (greeter), "timed-login", G_CALLBACK (timed_login_cb), web_view);
+
+    webkit_web_view_load_uri (WEBKIT_WEB_VIEW (web_view), "file:///home/bob/bzr/lightdm/index.html");
+    greeter_connect (greeter);
 
     gtk_widget_show_all (window);
 
