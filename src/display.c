3e6c9f121054aa36b922aaacb4322fc3f3ced939
robert.ancell@gmail.com <>
2010-07-29 23:06:12 +0200

    Note about having to change authorization after logout

--- src/display.c
+++ src/display.c
@@ -186,6 +186,8 @@ end_user_session (Display *display, gboolean clean_exit)
 
     if (!clean_exit)
         g_warning ("Session exited unexpectedly");
+  
+    // FIXME: Change authorization
 
     xserver_disconnect_clients (display->priv->xserver);
 }
