132c9ffb71da487b3e71386496bd4c09870a0e93
robert.ancell@gmail.com <>
2010-07-31 16:08:36 +0800

    Change libldmgreeter library from GPL to LGPL

--- libldmgreeter/greeter.h
+++ libldmgreeter/greeter.h
@@ -1,11 +1,11 @@
 /*
  * Copyright (C) 2010 Robert Ancell.
  * Author: Robert Ancell <robert.ancell@canonical.com>
- * 
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option) any later
- * version. See http://www.gnu.org/copyleft/gpl.html the full text of the
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 3 of the License, or (at your option) any
+ * later version. See http://www.gnu.org/copyleft/lgpl.html the full text of the
  * license.
  */
 
