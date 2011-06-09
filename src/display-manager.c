51c2b625f14a9951713c19773cd72f9d8f3d4725
Robert Ancell <robert.ancell@canonical.com>
2011-06-09 15:38:33 +1000

    Always write to ~/.xsession-errors even if in test mode

--- src/display-manager.c
+++ src/display-manager.c
@@ -156,21 +156,16 @@ start_session (Display *display, Session *session, gboolean is_greeter, DisplayM
     else
     {
         // FIXME: Copy old error file
-        if (getpid () != 0)
-            log_filename = g_strdup (".xsession-errors");
-        else
+        User *user = user_get_by_name (session_get_username (session));
+        if (user)
         {
-            User *user = user_get_by_name (session_get_username (session));
-            if (user)
-            {
-                log_filename = g_build_filename (user_get_home_directory (user), ".xsession-errors", NULL);
-                g_object_unref (user);
-            }
-            else
-                g_warning ("Failed to get user info for user '%s'", session_get_username (session));
+            log_filename = g_build_filename (user_get_home_directory (user), ".xsession-errors", NULL);
+            g_object_unref (user);
         }
+        else
+            g_warning ("Failed to get user info for user '%s'", session_get_username (session));
     }
-  
+
     if (log_filename)
     {      
         g_debug ("Logging to %s", log_filename);
