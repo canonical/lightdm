2ec30934264c43526fbf87cdf74c168d92a1e6ec
Robert Ancell <robert.ancell@canonical.com>
2011-06-09 16:21:25 +1000

    Keep session blocked until receives signal

--- tests/test-session.c
+++ tests/test-session.c
@@ -2,25 +2,29 @@
 #include <stdio.h>
 #include <signal.h>
 #include <xcb/xcb.h>
+#include <glib.h>
 
 #include "status.h"
 
 static void
 quit_cb (int signum)
 {
-    notify_status ("SESSION QUIT");
+    notify_status ("SESSION TERMINATE SIGNAL=%d", signum);
     exit (EXIT_SUCCESS);
 }
 
 int
 main (int argc, char **argv)
 {
+    GMainLoop *loop;
     xcb_connection_t *connection;
 
     signal (SIGINT, quit_cb);
     signal (SIGTERM, quit_cb);
 
     notify_status ("SESSION START USER=%s", getenv ("USER"));
+
+    loop = g_main_loop_new (NULL, FALSE);
 
     connection = xcb_connect (NULL, NULL);
 
@@ -31,6 +35,8 @@ main (int argc, char **argv)
     }
 
     notify_status ("SESSION CONNECT-XSERVER");
+
+    g_main_loop_run (loop);    
 
     return EXIT_SUCCESS;
 }
