9db615cc66bb0ea718948c2c7a7e9b05cd371f98
Robert Ancell <robert.ancell@canonical.com>
2011-09-06 15:46:30 +1000

    Rename AllowGuest to HasGuestAccount

--- src/lightdm.c
+++ src/lightdm.c
@@ -345,7 +345,7 @@ handle_seat_get_property (GDBusConnection       *connection,
 
     if (g_strcmp0 (property_name, "CanSwitch") == 0)
         result = g_variant_new_boolean (seat_get_can_switch (seat));
-    if (g_strcmp0 (property_name, "AllowGuest") == 0)
+    if (g_strcmp0 (property_name, "HasGuestAccount") == 0)
         result = g_variant_new_boolean (seat_get_allow_guest (seat));
     else if (g_strcmp0 (property_name, "Sessions") == 0)
     {
@@ -651,7 +651,7 @@ bus_acquired_cb (GDBusConnection *connection,
         "<node>"
         "  <interface name='org.freedesktop.DisplayManager.Seat'>"
         "    <property name='CanSwitch' type='b' access='read'/>"
-        "    <property name='AllowGuest' type='b' access='read'/>"
+        "    <property name='HasGuestAccount' type='b' access='read'/>"
         "    <property name='Sessions' type='ao' access='read'/>"
         "    <method name='SwitchToGreeter'/>"
         "    <method name='SwitchToUser'>"
