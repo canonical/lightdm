fe50efbbc89331d32653b9acb41e675e2cd24891
Matthew Fischer <matthew.fischer@canonical.com>
2012-06-05 21:31:03 -0600

    fixes 999713: setresuid/setresgid don't work on some platforms

--- src/privileges.c
+++ src/privileges.c
@@ -12,6 +12,7 @@
 /* for setres*id() */
 #define _GNU_SOURCE
 
+#include <config.h>
 #include "privileges.h"
 
 void
@@ -20,14 +21,38 @@ privileges_drop (User *user)
     g_return_if_fail (user != NULL);
 
     g_debug ("Dropping privileges to uid %i", user_get_uid (user));
+#ifdef HAVE_SETRESGID
+    g_debug ("Calling setresgid");
     g_assert (setresgid (user_get_gid (user), user_get_gid (user), -1) == 0);
+#else
+    g_assert (setgid (user_get_gid (user)) == 0);
+    g_assert (setegid (user_get_gid (user)) == 0);
+#endif
+#ifdef HAVE_SETRESUID
+    g_debug ("Calling setresuid");
     g_assert (setresuid (user_get_uid (user), user_get_uid (user), -1) == 0);
+#else
+    g_assert (setuid (user_get_uid (user)) == 0);
+    g_assert (seteuid (user_get_uid (user)) == 0);
+#endif
 }
 
 void
 privileges_reclaim (void)
 {
     g_debug ("Restoring privileges");
+#ifdef HAVE_SETRESUID
+    g_debug ("Calling setresuid");
     g_assert (setresuid (0, 0, -1) == 0);
-    g_assert (setresgid (0, 0, -1) == 0);  
+#else
+    g_assert (setuid (0) == 0);
+    g_assert (seteuid (0) == 0);
+#endif
+#ifdef HAVE_SETRESGID
+    g_debug ("Calling setresgid");
+    g_assert (setresgid (0, 0, -1) == 0);
+#else
+    g_assert (setgid (0) == 0);
+    g_assert (setegid (0) == 0);
+#endif
 }
