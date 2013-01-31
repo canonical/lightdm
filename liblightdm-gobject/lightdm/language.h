10d9e50401d1a82519470ef6281737748a5b27aa
Robert Ancell <robert.ancell@canonical.com>
2013-02-01 09:56:09 +1300

    Relicense liblightdm to LGPL-2/LGPL-3 so GPL-2 code can link against it

--- liblightdm-gobject/lightdm/language.h
+++ liblightdm-gobject/lightdm/language.h
@@ -1,12 +1,11 @@
 /*
  * Copyright (C) 2010 Robert Ancell.
  * Author: Robert Ancell <robert.ancell@canonical.com>
- * 
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
- * Software Foundation; either version 3 of the License, or (at your option) any
- * later version. See http://www.gnu.org/copyleft/lgpl.html the full text of the
- * license.
+ * Software Foundation; either version 2 or version 3 of the License.
+ * See http://www.gnu.org/copyleft/lgpl.html the full text of the license.
  */
 
 #ifndef _LIGHTDM_LANGUAGE_H_
