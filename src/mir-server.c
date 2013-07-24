daade887b6be4397d6caedc885e016669b2dfccf
Robert Ancell <robert.ancell@canonical.com>
2013-07-24 17:35:04 +1200

    Prepare for Mir servers having VTs

--- src/mir-server.c
+++ src/mir-server.c
@@ -15,7 +15,8 @@
 
 struct MirServerPrivate
 {
-    int dummy;
+    /* VT to run on */
+    gint vt;
 };
 
 G_DEFINE_TYPE (MirServer, mir_server, DISPLAY_SERVER_TYPE);
@@ -25,6 +26,13 @@ MirServer *mir_server_new (void)
     return g_object_new (MIR_SERVER_TYPE, NULL);  
 }
 
+static gint
+mir_server_local_get_vt (DisplayServer *server)
+{
+    g_return_val_if_fail (server != NULL, 0);
+    return MIR_SERVER (server)->priv->vt;
+}
+
 static gboolean
 mir_server_start (DisplayServer *display_server)
 {
@@ -32,9 +40,15 @@ mir_server_start (DisplayServer *display_server)
 }
 
 static void
+mir_server_setup_session (DisplayServer *display_server, Session *session)
+{
+}
+
+static void
 mir_server_init (MirServer *server)
 {
     server->priv = G_TYPE_INSTANCE_GET_PRIVATE (server, MIR_SERVER_TYPE, MirServerPrivate);
+    server->priv->vt = -1;
 }
 
 static void
@@ -49,7 +63,9 @@ mir_server_class_init (MirServerClass *klass)
     GObjectClass *object_class = G_OBJECT_CLASS (klass);
     DisplayServerClass *display_server_class = DISPLAY_SERVER_CLASS (klass);
 
+    display_server_class->get_vt = mir_server_local_get_vt;
     display_server_class->start = mir_server_start;
+    display_server_class->setup_session = mir_server_setup_session;
     object_class->finalize = mir_server_finalize;
 
     g_type_class_add_private (klass, sizeof (MirServerPrivate));
