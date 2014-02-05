9fd91a4edb59bd10d9d59a88322d81e657740794
Alberts Muktupāvels <alberts.muktupavels@gmail.com>
2014-02-05 16:29:02 +0200

    simplify and re-add removed VT check

--- src/seat-xlocal.c
+++ src/seat-xlocal.c
@@ -79,7 +79,7 @@ get_vt (Seat *seat, DisplayServer *display_server)
     }
     if (plymouth_get_is_active ())
         plymouth_quit (FALSE);
-    if (vt < 0 && (!xdg_seat || (xdg_seat && strcmp (xdg_seat, "seat0") == 0)))
+    if (vt < 0 && g_strcmp0 (xdg_seat, "seat0") == 0)
         vt = vt_get_unused ();
 
     return vt;
@@ -259,9 +259,7 @@ static void
 seat_xlocal_set_active_session (Seat *seat, Session *session)
 {
     gint vt = display_server_get_vt (session_get_display_server (session));
-    const gchar * xdg_seat = seat_get_string_property (seat, "xdg-seat");
-    
-    if (vt >= 0 && (!xdg_seat || (xdg_seat && strcmp (xdg_seat, "seat0") == 0)))
+    if (vt >= 0)
         vt_set_active (vt);
 
     SEAT_CLASS (seat_xlocal_parent_class)->set_active_session (seat, session);
@@ -274,6 +272,8 @@ seat_xlocal_get_active_session (Seat *seat)
     GList *link;
 
     vt = vt_get_active ();
+    if (vt < 0)
+    	return NULL;
 
     for (link = seat_get_sessions (seat); link; link = link->next)
     {
