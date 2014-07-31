971e5ce612c33d2840a144245cddd6978169811b
Robert Ancell <robert.ancell@canonical.com>
2014-07-31 16:51:07 +1200

    Return correct errors for D-Bus calls

--- src/lightdm.c
+++ src/lightdm.c
@@ -321,7 +321,10 @@ handle_display_manager_call (GDBusConnection       *connection,
         Seat *seat;
 
         if (!g_variant_is_of_type (parameters, G_VARIANT_TYPE ("(sa(ss))")))
+        {
+            g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_INVALID_ARGS, "Invalid arguments");
             return;
+        }
 
         g_variant_get (parameters, "(&sa(ss))", &type, &property_iter);
 
@@ -360,7 +363,10 @@ handle_display_manager_call (GDBusConnection       *connection,
         Seat *seat;
 
         if (!g_variant_is_of_type (parameters, G_VARIANT_TYPE ("(i)")))
+        {
+            g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_INVALID_ARGS, "Invalid arguments");
             return;
+        }
 
         g_variant_get (parameters, "(i)", &display_number);
 
@@ -395,6 +401,8 @@ handle_display_manager_call (GDBusConnection       *connection,
             g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_FAILED, "Failed to start seat");
         g_object_unref (seat);
     }
+    else
+        g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_METHOD, "Unknown method");
 }
 
 static GVariant *
@@ -438,45 +446,58 @@ handle_seat_call (GDBusConnection       *connection,
     if (g_strcmp0 (method_name, "SwitchToGreeter") == 0)
     {
         if (!g_variant_is_of_type (parameters, G_VARIANT_TYPE ("()")))
-            return;
+            g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_INVALID_ARGS, "Invalid arguments");
 
-        seat_switch_to_greeter (seat);
-        g_dbus_method_invocation_return_value (invocation, NULL);
+        if (seat_switch_to_greeter (seat))
+            g_dbus_method_invocation_return_value (invocation, NULL);
+        else// FIXME: Need to make proper error
+            g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_FAILED, "Failed to switch to greeter");
     }
     else if (g_strcmp0 (method_name, "SwitchToUser") == 0)
     {
         const gchar *username, *session_name;
 
         if (!g_variant_is_of_type (parameters, G_VARIANT_TYPE ("(ss)")))
-            return;
+            g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_INVALID_ARGS, "Invalid arguments");
 
         g_variant_get (parameters, "(&s&s)", &username, &session_name);
         if (strcmp (session_name, "") == 0)
             session_name = NULL;
 
-        seat_switch_to_user (seat, username, session_name);
-        g_dbus_method_invocation_return_value (invocation, NULL);
+        if (seat_switch_to_user (seat, username, session_name))
+            g_dbus_method_invocation_return_value (invocation, NULL);
+        else// FIXME: Need to make proper error
+            g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_FAILED, "Failed to switch to user");
     }
     else if (g_strcmp0 (method_name, "SwitchToGuest") == 0)
     {
         const gchar *session_name;
 
         if (!g_variant_is_of_type (parameters, G_VARIANT_TYPE ("(s)")))
-            return;
+            g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_INVALID_ARGS, "Invalid arguments");
 
         g_variant_get (parameters, "(&s)", &session_name);
         if (strcmp (session_name, "") == 0)
             session_name = NULL;
 
-        seat_switch_to_guest (seat, session_name);
-        g_dbus_method_invocation_return_value (invocation, NULL);
+        if (seat_switch_to_guest (seat, session_name))
+            g_dbus_method_invocation_return_value (invocation, NULL);
+        else// FIXME: Need to make proper error
+            g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_FAILED, "Failed to switch to guest");
     }
     else if (g_strcmp0 (method_name, "Lock") == 0)
     {
+        if (!g_variant_is_of_type (parameters, G_VARIANT_TYPE ("()")))
+            g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_INVALID_ARGS, "Invalid arguments");
+
         /* FIXME: Should only allow locks if have a session on this seat */
-        seat_lock (seat, NULL);
-        g_dbus_method_invocation_return_value (invocation, NULL);
+        if (seat_lock (seat, NULL))
+            g_dbus_method_invocation_return_value (invocation, NULL);
+        else// FIXME: Need to make proper error
+            g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_FAILED, "Failed to lock seat");
     }
+    else
+        g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_METHOD, "Unknown method");
 }
 
 static Seat *
@@ -538,11 +559,16 @@ handle_session_call (GDBusConnection       *connection,
     {
         Seat *seat;
 
+        if (!g_variant_is_of_type (parameters, G_VARIANT_TYPE ("()")))
+            g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_INVALID_ARGS, "Invalid arguments");
+
         seat = get_seat_for_session (session);
         /* FIXME: Should only allow locks if have a session on this seat */
         seat_lock (seat, session_get_username (session));
         g_dbus_method_invocation_return_value (invocation, NULL);
     }
+    else
+        g_dbus_method_invocation_return_error (invocation, G_DBUS_ERROR, G_DBUS_ERROR_UNKNOWN_METHOD, "Unknown method");
 }
 
 static SeatBusEntry *
