1f1d796561faf999a8e9e8586641ecf67b45c595
Robert Ancell <robert.ancell@canonical.com>
2014-09-04 16:23:06 +1200

    Convert remaining login1 functions into methods in Login1Service

--- src/login1.c
+++ src/login1.c
@@ -73,129 +73,6 @@ G_DEFINE_TYPE (Login1Seat, login1_seat, G_TYPE_OBJECT);
 
 static Login1Service *singleton = NULL;
 
-void
-login1_lock_session (const gchar *session_id)
-{
-    GDBusConnection *bus;
-    GError *error = NULL;
-
-    g_return_if_fail (session_id != NULL);
-
-    g_debug ("Locking login1 session %s", session_id);
-
-    bus = g_bus_get_sync (G_BUS_TYPE_SYSTEM, NULL, &error);
-    if (error)
-        g_warning ("Failed to get system bus: %s", error->message);
-    g_clear_error (&error);
-    if (!bus)
-        return;
-
-    if (session_id)
-    {
-        GVariant *result;
-
-        result = g_dbus_connection_call_sync (bus,
-                                              LOGIN1_SERVICE_NAME,
-                                              LOGIN1_OBJECT_NAME,
-                                              LOGIN1_MANAGER_INTERFACE_NAME,
-                                              "LockSession",
-                                              g_variant_new ("(s)", session_id),
-                                              G_VARIANT_TYPE ("()"),
-                                              G_DBUS_CALL_FLAGS_NONE,
-                                              -1,
-                                              NULL,
-                                              &error);
-        if (error)
-            g_warning ("Error locking login1 session: %s", error->message);
-        g_clear_error (&error);
-        if (result)
-            g_variant_unref (result);
-    }
-    g_object_unref (bus);
-}
-
-void
-login1_unlock_session (const gchar *session_id)
-{
-    GDBusConnection *bus;
-    GError *error = NULL;
-
-    g_return_if_fail (session_id != NULL);
-
-    g_debug ("Unlocking login1 session %s", session_id);
-
-    bus = g_bus_get_sync (G_BUS_TYPE_SYSTEM, NULL, &error);
-    if (error)
-        g_warning ("Failed to get system bus: %s", error->message);
-    g_clear_error (&error);
-    if (!bus)
-        return;
-
-    if (session_id)
-    {
-        GVariant *result;
-
-        result = g_dbus_connection_call_sync (bus,
-                                              LOGIN1_SERVICE_NAME,
-                                              LOGIN1_OBJECT_NAME,
-                                              LOGIN1_MANAGER_INTERFACE_NAME,
-                                              "UnlockSession",
-                                              g_variant_new ("(s)", session_id),
-                                              G_VARIANT_TYPE ("()"),
-                                              G_DBUS_CALL_FLAGS_NONE,
-                                              -1,
-                                              NULL,
-                                              &error);
-        if (error)
-            g_warning ("Error unlocking login1 session: %s", error->message);
-        g_clear_error (&error);
-        if (result)
-            g_variant_unref (result);
-    }
-    g_object_unref (bus);
-}
-
-void
-login1_activate_session (const gchar *session_id)
-{
-    GDBusConnection *bus;
-    GError *error = NULL;
-
-    g_return_if_fail (session_id != NULL);
-
-    g_debug ("Activating login1 session %s", session_id);
-
-    bus = g_bus_get_sync (G_BUS_TYPE_SYSTEM, NULL, &error);
-    if (error)
-        g_warning ("Failed to get system bus: %s", error->message);
-    g_clear_error (&error);
-    if (!bus)
-        return;
-
-    if (session_id)
-    {
-        GVariant *result;
-
-        result = g_dbus_connection_call_sync (bus,
-                                              LOGIN1_SERVICE_NAME,
-                                              LOGIN1_OBJECT_NAME,
-                                              LOGIN1_MANAGER_INTERFACE_NAME,
-                                              "ActivateSession",
-                                              g_variant_new ("(s)", session_id),
-                                              G_VARIANT_TYPE ("()"),
-                                              G_DBUS_CALL_FLAGS_NONE,
-                                              -1,
-                                              NULL,
-                                              &error);
-        if (error)
-            g_warning ("Error activating login1 session: %s", error->message);
-        g_clear_error (&error);
-        if (result)
-            g_variant_unref (result);
-    }
-    g_object_unref (bus);
-}
-
 Login1Service *
 login1_service_get_instance (void)
 {
@@ -445,6 +322,105 @@ login1_service_get_seat (Login1Service *service, const gchar *id)
     }
 
     return NULL;
+}
+
+void
+login1_service_lock_session (Login1Service *service, const gchar *session_id)
+{
+    GError *error = NULL;
+
+    g_return_if_fail (service != NULL);
+    g_return_if_fail (session_id != NULL);
+
+    g_debug ("Locking login1 session %s", session_id);
+
+    if (session_id)
+    {
+        GVariant *result;
+
+        result = g_dbus_connection_call_sync (service->priv->connection,
+                                              LOGIN1_SERVICE_NAME,
+                                              LOGIN1_OBJECT_NAME,
+                                              LOGIN1_MANAGER_INTERFACE_NAME,
+                                              "LockSession",
+                                              g_variant_new ("(s)", session_id),
+                                              G_VARIANT_TYPE ("()"),
+                                              G_DBUS_CALL_FLAGS_NONE,
+                                              -1,
+                                              NULL,
+                                              &error);
+        if (error)
+            g_warning ("Error locking login1 session: %s", error->message);
+        g_clear_error (&error);
+        if (result)
+            g_variant_unref (result);
+    }
+}
+
+void
+login1_service_unlock_session (Login1Service *service, const gchar *session_id)
+{
+    GError *error = NULL;
+
+    g_return_if_fail (service != NULL);
+    g_return_if_fail (session_id != NULL);
+
+    g_debug ("Unlocking login1 session %s", session_id);
+
+    if (session_id)
+    {
+        GVariant *result;
+
+        result = g_dbus_connection_call_sync (service->priv->connection,
+                                              LOGIN1_SERVICE_NAME,
+                                              LOGIN1_OBJECT_NAME,
+                                              LOGIN1_MANAGER_INTERFACE_NAME,
+                                              "UnlockSession",
+                                              g_variant_new ("(s)", session_id),
+                                              G_VARIANT_TYPE ("()"),
+                                              G_DBUS_CALL_FLAGS_NONE,
+                                              -1,
+                                              NULL,
+                                              &error);
+        if (error)
+            g_warning ("Error unlocking login1 session: %s", error->message);
+        g_clear_error (&error);
+        if (result)
+            g_variant_unref (result);
+    }
+}
+
+void
+login1_service_activate_session (Login1Service *service, const gchar *session_id)
+{
+    GError *error = NULL;
+
+    g_return_if_fail (service != NULL);
+    g_return_if_fail (session_id != NULL);
+
+    g_debug ("Activating login1 session %s", session_id);
+
+    if (session_id)
+    {
+        GVariant *result;
+
+        result = g_dbus_connection_call_sync (service->priv->connection,
+                                              LOGIN1_SERVICE_NAME,
+                                              LOGIN1_OBJECT_NAME,
+                                              LOGIN1_MANAGER_INTERFACE_NAME,
+                                              "ActivateSession",
+                                              g_variant_new ("(s)", session_id),
+                                              G_VARIANT_TYPE ("()"),
+                                              G_DBUS_CALL_FLAGS_NONE,
+                                              -1,
+                                              NULL,
+                                              &error);
+        if (error)
+            g_warning ("Error activating login1 session: %s", error->message);
+        g_clear_error (&error);
+        if (result)
+            g_variant_unref (result);
+    }
 }
 
 static void
