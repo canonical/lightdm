f99d967d5f786686b5735317ac801daa1a361dfd
Robert Ancell <robert.ancell@canonical.com>
2015-11-20 13:25:53 +1300

    Backport XDMCP improvements, including: - Don't start LightDM if the XDMCP server is configured with a key that doesn't exist - Add IP addresses to XDMCP log messages - Refactor XDMCP error handling - Adding many more tests

--- tests/src/xdmcp-client.h
+++ tests/src/xdmcp-client.h
@@ -7,11 +7,12 @@
 #define XDMCP_VERSION 1
 #define XDMCP_PORT 177
 
-#define XDMCP_CLIENT_SIGNAL_QUERY   "query"
-#define XDMCP_CLIENT_SIGNAL_WILLING "willing"
-#define XDMCP_CLIENT_SIGNAL_ACCEPT  "accept"
-#define XDMCP_CLIENT_SIGNAL_DECLINE "decline"
-#define XDMCP_CLIENT_SIGNAL_FAILED  "failed"
+#define XDMCP_CLIENT_SIGNAL_WILLING    "willing"
+#define XDMCP_CLIENT_SIGNAL_UNWILLING  "unwilling"
+#define XDMCP_CLIENT_SIGNAL_ACCEPT     "accept"
+#define XDMCP_CLIENT_SIGNAL_DECLINE    "decline"
+#define XDMCP_CLIENT_SIGNAL_FAILED     "failed"
+#define XDMCP_CLIENT_SIGNAL_ALIVE      "alive"
 
 typedef struct
 {
@@ -22,8 +23,16 @@ typedef struct
 
 typedef struct
 {
+    gchar *hostname;
+    gchar *status;
+} XDMCPUnwilling;
+
+typedef struct
+{
     guint32 session_id;
     gchar *authentication_name;
+    guint16 authentication_data_length;
+    guint8 *authentication_data;
     gchar *authorization_name;
     guint16 authorization_data_length;
     guint8 *authorization_data;
@@ -33,6 +42,8 @@ typedef struct
 {
     gchar *status;
     gchar *authentication_name;
+    guint16 authentication_data_length;
+    guint8 *authentication_data;
 } XDMCPDecline;
 
 typedef struct
@@ -40,6 +51,12 @@ typedef struct
     guint32 session_id;
     gchar *status;
 } XDMCPFailed;
+
+typedef struct
+{
+    gboolean session_running;
+    guint32 session_id;
+} XDMCPAlive;
 
 typedef struct XDMCPClientPrivate XDMCPClientPrivate;
 
@@ -52,11 +69,12 @@ typedef struct
 typedef struct
 {
    GObjectClass parent_class;
-   void (*query)(XDMCPClient *client);
    void (*willing)(XDMCPClient *client, XDMCPWilling *message);
+   void (*unwilling)(XDMCPClient *client, XDMCPUnwilling *message);  
    void (*accept)(XDMCPClient *client, XDMCPAccept *message);
    void (*decline)(XDMCPClient *client, XDMCPDecline *message);
    void (*failed)(XDMCPClient *client, XDMCPFailed *message);
+   void (*alive)(XDMCPClient *client, XDMCPAlive *message);
 } XDMCPClientClass;
 
 GType xdmcp_client_get_type (void);
@@ -71,7 +89,11 @@ gboolean xdmcp_client_start (XDMCPClient *client);
 
 GInetAddress *xdmcp_client_get_local_address (XDMCPClient *client);
 
-void xdmcp_client_send_query (XDMCPClient *client);
+void xdmcp_client_send_query (XDMCPClient *client, gchar **authentication_names);
+
+void xdmcp_client_send_broadcast_query (XDMCPClient *client, gchar **authentication_names);
+
+void xdmcp_client_send_indirect_query (XDMCPClient *client, gchar **authentication_names);
 
 void xdmcp_client_send_request (XDMCPClient *client,
                                 guint16 display_number,
@@ -80,7 +102,9 @@ void xdmcp_client_send_request (XDMCPClient *client,
                                 const guint8 *authentication_data, guint16 authentication_data_length,
                                 gchar **authorization_names, const gchar *mfid);
 
-void xdmcp_client_send_manage (XDMCPClient *client, guint32 session_id, guint16 display_number, gchar *display_class);
+void xdmcp_client_send_manage (XDMCPClient *client, guint32 session_id, guint16 display_number, const gchar *display_class);
+
+void xdmcp_client_send_keep_alive (XDMCPClient *client, guint16 display_number, guint32 session_id);
 
 G_END_DECLS
 
