canonical/lightdm

commit 77a7c6b7b8ca896b98ef43826641bdd520650bfb

config: set `logind-check-graphical=True` per default
Without `logind-check-graphical=True` beeing set, lightdm is prone to a race condition, where lightdm has started before the graphics driver was loaded.

src/lightdm.c
@@ -812,6 +812,8 @@ main (int argc, char **argv)
     }
     if (!config_has_key (config_get_instance (), "XDMCPServer", "hostname"))
         config_set_string (config_get_instance (), "XDMCPServer", "hostname", g_get_host_name ());
+    if (!config_has_key (config_get_instance (), "LightDM", "logind-check-graphical"))
+        config_set_string (config_get_instance (), "LightDM", "logind-check-graphical", TRUE);
 
     /* Override defaults */
     if (log_dir)

data/lightdm.conf
@@ -26,7 +26,7 @@
 #lock-memory=true
 #user-authority-in-system-dir=false
 #guest-account-script=guest-account
-#logind-check-graphical=false
+#logind-check-graphical=true
 #log-directory=/var/log/lightdm
 #run-directory=/var/run/lightdm
 #cache-directory=/var/cache/lightdm

commit 3fd8d5c0d909d4b7e8cab285a08410796cc69e69

Fix crash due to using wrong method to set configuration
Regression introduced in 77a7c6b7b8ca896b98ef43826641bdd520650bfb

src/lightdm.c
@@ -813,7 +813,7 @@ main (int argc, char **argv)
     if (!config_has_key (config_get_instance (), "XDMCPServer", "hostname"))
         config_set_string (config_get_instance (), "XDMCPServer", "hostname", g_get_host_name ());
     if (!config_has_key (config_get_instance (), "LightDM", "logind-check-graphical"))
-        config_set_string (config_get_instance (), "LightDM", "logind-check-graphical", TRUE);
+        config_set_boolean (config_get_instance (), "LightDM", "logind-check-graphical", TRUE);
 
     /* Override defaults */
     if (log_dir)