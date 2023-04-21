899cc3a51619f81c808cfb2b62ff11dc7d5b478a
Richard Hansen <rhansen@rhansen.org>
2023-04-21 17:46:47 -0400

    tests: Include $DBUS_SYSTEM_BUS_ADDRESS in the test log

--- tests/src/test-runner.c
+++ tests/src/test-runner.c
@@ -467,8 +467,8 @@ handle_command (const gchar *command)
             g_string_append (command_line, " --debug");
         g_string_append_printf (command_line, " --cache-dir %s/cache", temp_dir);
 
-        test_runner_command = g_strdup_printf ("PATH=%s LD_PRELOAD=%s LD_LIBRARY_PATH=%s LIGHTDM_TEST_ROOT=%s DBUS_SESSION_BUS_ADDRESS=%s %s\n",
-                                               g_getenv ("PATH"), g_getenv ("LD_PRELOAD"), g_getenv ("LD_LIBRARY_PATH"), g_getenv ("LIGHTDM_TEST_ROOT"), g_getenv ("DBUS_SESSION_BUS_ADDRESS"),
+        test_runner_command = g_strdup_printf ("PATH=%s LD_PRELOAD=%s LD_LIBRARY_PATH=%s LIGHTDM_TEST_ROOT=%s DBUS_SESSION_BUS_ADDRESS=%s DBUS_SYSTEM_BUS_ADDRESS=%s %s\n",
+                                               g_getenv ("PATH"), g_getenv ("LD_PRELOAD"), g_getenv ("LD_LIBRARY_PATH"), g_getenv ("LIGHTDM_TEST_ROOT"), g_getenv ("DBUS_SESSION_BUS_ADDRESS"), g_getenv ("DBUS_SYSTEM_BUS_ADDRESS"),
                                                command_line->str);
 
         gchar **lightdm_argv;
