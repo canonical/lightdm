de26335f84fa8e74b097684280ce836aa45c1389
Guido Falsi <mad@madpilot.net>
2023-12-22 22:42:35 +0100

    Add "smart-xsession-errors" configuration
    This allows to choose the logic for .xsession-errors placement.

--- src/lightdm.c
+++ src/lightdm.c
@@ -771,6 +771,8 @@ main (int argc, char **argv)
         config_set_boolean (config_get_instance (), "LightDM", "backup-logs", TRUE);
     if (!config_has_key (config_get_instance (), "LightDM", "dbus-service"))
         config_set_boolean (config_get_instance (), "LightDM", "dbus-service", TRUE);
+    if (!config_has_key (config_get_instance (), "LightDM", "smart-xsession-errors"))
+        config_set_boolean (config_get_instance (), "LightDM", "smart-xsession-errors", FALSE);
     if (!config_has_key (config_get_instance (), "Seat:*", "type"))
         config_set_string (config_get_instance (), "Seat:*", "type", "local");
     if (!config_has_key (config_get_instance (), "Seat:*", "pam-service"))
